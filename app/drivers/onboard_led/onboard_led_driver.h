cf939c16e15654e59dd10fde2c2523943ce4a666
Leandro Perez Guatibonza <leandropg@gmail.com>
2026-05-12 15:44:10 +0200

    Fix issue to call C code from C++

--- app/drivers/onboard_led/onboard_led_driver.h
+++ app/drivers/onboard_led/onboard_led_driver.h
@@ -1,6 +1,10 @@
 #ifndef ONBOARD_LED_DRIVER_H_
 #define ONBOARD_LED_DRIVER_H_
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 /**
  * @brief Define Data Struct
  */
@@ -15,5 +19,9 @@ struct onboard_led_driver_data {
  * @param count_value Count Value
  */
 void onboard_led_set_blink_counter(const struct device *dev, int count_value);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif // ONBOARD_LED_DRIVER_H_
