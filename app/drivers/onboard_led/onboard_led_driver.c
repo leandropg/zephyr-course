dc63c822e31585c79ddd58e4f1d5a5378dec3f46
Leandro Perez Guatibonza <leandropg@gmail.com>
2026-05-12 11:32:44 +0200

    Basic Sensor Get implemention

---
+++ app/drivers/onboard_led/onboard_led_driver.c
@@ -0,0 +1,35 @@
+#include <zephyr/drivers/sensor.h>
+#include <zephyr/logging/log.h>
+
+#define DT_DRV_COMPAT onboard_led_driver
+
+// Logger Declaration
+LOG_MODULE_REGISTER(onboard_led, LOG_LEVEL_INF);
+
+/**
+ * @brief Channel Get Onboard Led Implementation
+ */
+static int channel_get_onboard_led(const struct device *dev,
+                                        enum sensor_channel chan,
+                                        struct sensor_value *val) {
+    LOG_INF("My Sensor Channel GET");
+    return 0;
+}
+
+static DEVICE_API(sensor, onboard_led_driver_api) = {
+
+    .channel_get = channel_get_onboard_led,
+};
+
+/**
+ * @brief Onboard Led Init Function
+ * @param dev Device Pointer
+ */
+static int onboard_led_init(const struct device* dev) {
+
+    LOG_INF("Onboard Led Driver initialized");
+    return 0;
+}
+
+// Instance Onboard Led
+DEVICE_DT_INST_DEFINE(0, onboard_led_init, NULL, NULL, NULL, POST_KERNEL, 80, &onboard_led_driver_api);
--- app/src/main.cpp
+++ app/src/main.cpp
@@ -1,4 +1,5 @@
 #include <zephyr/drivers/gpio.h>
+#include <zephyr/drivers/sensor.h>
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
 
@@ -8,6 +9,15 @@
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED_NODE, gpios);
 
 LOG_MODULE_REGISTER(main, LOG_LEVEL_INF);
+
+int test_onboard_led_get() {
+
+    const struct device* my_sensor_dev = DEVICE_DT_GET(DT_NODELABEL(onboard_led0));
+    struct sensor_value val;
+    int error_code = sensor_channel_get(my_sensor_dev, SENSOR_CHAN_AMBIENT_TEMP, &val);
+    LOG_INF("Get Channel. Error Code: %d", error_code);
+    return 0;
+}
 
 int main(void)
 {
@@ -23,6 +33,9 @@ int main(void)
         led_state = !led_state;
         LOG_INF("LED state: %s", led_state ? "ON" : "OFF");
         k_msleep(CONFIG_APP_HEARTBEAT_PERIOD_MS);
+
+        // Test Onboard Led Get
+        test_onboard_led_get();
     }
     return 0;
 }
